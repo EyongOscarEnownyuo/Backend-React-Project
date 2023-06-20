449cf0c40a2792ba789fa234981fbaf558b02ea5
EyongOscarEnownyuo <133630788+EyongOscarEnownyuo@users.noreply.github.com>
2023-06-20 20:12:14 +0200

    Create app.config.js

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+   DB: 'mongodb+srv://eyongoscar0:pdtpopsi@backend.ppjzoee.mongodb.net/'
+    
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
